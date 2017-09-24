f91b2187bf47a79d59a14f9efaadc915e71961a4
faridfedora <faridfedora@gmail.com>
2017-09-24 04:23:42 +0330

    Add project files.

---
+++ ionic-js-sidemenu1/platforms/android/assets/www/js/services.js
@@ -0,0 +1,19 @@
+angular
+    .module('comicsApp')
+    .factory('Comics', function($http) {
+        //var dataSource = 'http://samcroft.co.uk/comics-app/comics?callback=JSON_CALLBACK';
+        var dataSource = 'http://faridheydari.ir/webService/getPosts.php';
+
+        return {
+            getComics: function() {
+                return $http.jsonp(dataSource);
+            },
+            getComic: function(comicId) {
+                return $http.jsonp(dataSource, {
+                    params: {
+                        id: comicId
+                    }
+                });
+            }
+        }
+    });
